115f021ab3d991d87cdfb2c8f008deec4e8545ea
Tuomo <tuokri@tuta.io>
2019-11-07 20:53:37 +0200

    Connect to RS2 Web Admin

--- autoban.py
+++ autoban.py
@@ -1,6 +1,7 @@
 import os
 import time
 
+from rs2wapy import RS2WebAdmin
 from rs2wat import FTPCollector
 from rs2wat import db
 
@@ -8,17 +9,23 @@ FTP_HOST = os.environ["FTP_HOST"]
 FTP_PORT = os.environ["FTP_PORT"]
 FTP_USERNAME = os.environ["FTP_USERNAME"]
 FTP_PASSWORD = os.environ["FTP_PASSWORD"]
-
+WA_USERNAME = os.environ["WA_USERNAME"]
+WA_PASSWORD = os.environ["WA_PASSWORD"]
+WA_URL = os.environ["WA_URL"]
 DATABASE_URL = os.environ["DATABASE_URL"]
+
+LOG_IP_REGEX = r"(.*)\s((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"
 
 
 def main():
     db.init_db(DATABASE_URL)
     ftpc = FTPCollector(FTP_HOST, FTP_PORT, FTP_USERNAME, FTP_PASSWORD)
+    wa = RS2WebAdmin(WA_USERNAME, WA_PASSWORD, WA_URL)
 
     while True:
         new_m = ftpc.get_new_modifications("/81.19.210.136_7877/ROGame/Logs/Launch.log")
         print(f"got {len(new_m)} new modifications")
+
         time.sleep(1)
 
 
