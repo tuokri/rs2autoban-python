554011ac555d1fa464cf0b0d2f12742ec154aa08
Tuomo <tuokri@tuta.io>
2019-11-07 19:54:11 +0200

    Initial version

---
+++ autoban.py
@@ -0,0 +1,22 @@
+import os
+
+FTP_HOST = os.environ["FTP_HOST"]
+FTP_PORT = os.environ["FTP_PORT"]
+FTP_USERNAME = os.environ["FTP_USERNAME"]
+FTP_PASSWORD = os.environ["FTP_PASSWORD"]
+
+DATABASE_URL = os.environ["DATABASE_URL"]
+
+from rs2wat import FTPCollector
+from rs2wat import db
+
+
+def main():
+    db.init_db(DATABASE_URL)
+    ftpc = FTPCollector(FTP_HOST, FTP_PORT, FTP_USERNAME, FTP_PASSWORD)
+
+    print(ftpc.get_new_modifications("Launch.log"))
+
+
+if __name__ == '__main__':
+    main()
