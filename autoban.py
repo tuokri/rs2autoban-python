9af69fd5f207a9a7a6efde4ed95c41bbd00dbd0f
Tuomo <tuokri@tuta.io>
2019-11-07 21:07:23 +0200

    Improve IP matching

--- autoban.py
+++ autoban.py
@@ -15,27 +15,44 @@ WA_PASSWORD = os.environ["WA_PASSWORD"]
 WA_URL = os.environ["WA_URL"]
 DATABASE_URL = os.environ["DATABASE_URL"]
 
-LOG_IP_REGEX = r"(.*)\s((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))"
+LOG_IP_REGEX = (r"(.*)\s((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)"
+                r"{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)).*PlayerName:\s(.*)")
 
 
 def main():
     db.init_db(DATABASE_URL)
     ftpc = FTPCollector(FTP_HOST, FTP_PORT, FTP_USERNAME, FTP_PASSWORD)
     wa = RS2WebAdmin(WA_USERNAME, WA_PASSWORD, WA_URL)
-    ips = set()
+    ips = {}
 
     while True:
         new_m = ftpc.get_new_modifications("/81.19.210.136_7877/ROGame/Logs/Launch.log")
         print(f"got {len(new_m)} new modifications")
         new_m = "\n".join(new_m)
-        print(new_m)
         print(f"joined modification string length: {len(new_m)}")
 
         it = re.finditer(LOG_IP_REGEX, new_m)
         for i in it:
-            ip = i.groups()[1]
-            ips.add(ip)
+            groups = i.groups()
+            ip = groups[1]
+            name = None
+            try:
+                name = groups[1]
+            except IndexError:
+                pass
 
+            if ip not in ips:
+                ips[ip] = set()
+
+            if name:
+                try:
+                    ips[ip].remove(None)
+                except KeyError:
+                    pass
+
+            ips[ip].add(name)
+
+        print(ips)
         time.sleep(1)
 
 
