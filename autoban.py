388861052cc9027fb10c63e541d8a4564f29cd17
Tuomo <tuokri@tuta.io>
2019-11-07 21:23:53 +0200

    Autoban IPs, which have no player name after a grace period

--- autoban.py
+++ autoban.py
@@ -1,6 +1,7 @@
 import os
 import re
 import time
+from typing import List
 
 from rs2wapy import RS2WebAdmin
 from rs2wat import FTPCollector
@@ -17,6 +18,42 @@ DATABASE_URL = os.environ["DATABASE_URL"]
 
 LOG_IP_REGEX = (r"(.*)\s((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.)"
                 r"{3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)).*PlayerName:\s(.*)")
+GRACE_PERIOD = 30
+
+
+def get_suspicious_ips(ip_dict: dict) -> List[str]:
+    susp = []
+    for ip, name in ip_dict:
+        if name is None:
+            print(f"found suspicious ip: {ip}")
+            susp.append(ip)
+    return susp
+
+
+def check_grace_periods(ips: List[str], timers: dict, wa: RS2WebAdmin):
+    for ip in ips:
+        if ip not in timers:
+            t = time.time()
+            timers[ip] = t
+            print(f"starting grace period timer for: {ip}: {t}")
+
+    for ip, start_time in timers.items():
+        if ip not in ips:
+            print(f"removing no longer suspicious ip: {ip}")
+            timers.pop(ip)
+        else:
+            if time.time() > (start_time + GRACE_PERIOD):
+                print(f"grace period expired for {ip}")
+                policy = wa.get_access_policy()
+                already_banned = False
+                for p in policy:
+                    if ip in p:
+                        already_banned = True
+                        print(f"{ip} already banned in WebAdmin")
+                        break
+                if not already_banned:
+                    print(f"banning: {ip}")
+                    wa.add_access_policy(ip, "DENY")
 
 
 def main():
@@ -24,6 +61,7 @@ def main():
     ftpc = FTPCollector(FTP_HOST, FTP_PORT, FTP_USERNAME, FTP_PASSWORD)
     wa = RS2WebAdmin(WA_USERNAME, WA_PASSWORD, WA_URL)
     ips = {}
+    timers = {}
 
     while True:
         new_m = ftpc.get_new_modifications("/81.19.210.136_7877/ROGame/Logs/Launch.log")
@@ -52,7 +90,9 @@ def main():
 
             ips[ip].add(name)
 
-        print(ips)
+        susp = get_suspicious_ips(ips)
+        check_grace_periods(susp, timers)
+
         time.sleep(1)
 
 
