30e0fba65a45c96a58ef0587f11977877fbfe1d8
Tuomo <tuokri@users.noreply.github.com>
2020-07-23 19:57:39 +0000

    Update heartbeat.py

--- heartbeat.py
+++ heartbeat.py
@@ -45,9 +45,9 @@ def main():
                 min_ = min(pings)
                 max_ = max(pings)
                 emoji = ":white_check_mark:" if avg <= 200 else ":warning:"
-                msg = (f"{emoji} [{datetime.datetime.now().isoformat()}]: "
+                msg = (f"{emoji} `[{datetime.datetime.now().isoformat()}]: "
                        f"{num_players} players. "
-                       f"Ping min=**{min_}**, max=**{max_}**, avg=**{avg:.2f}**.")
+                       f"Ping min=**{min_}**, max=**{max_}**, avg=**{avg:.2f}**.`")
                 wh.post_chat_message(msg)
         except Exception as e:
             write_exception(e)
